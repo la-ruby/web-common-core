06e2b78d0a23178824c1397c01dab8448f9e0b07
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-05-26 00:44:34 +0000

    Installed bootstrap and jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,14 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// Make $ available on the window object
+// for SJR views and jQuery plugins
+// that may expect `$` to be globally available.
+require('expose-loader?$!jquery')
+
+// js
+import 'bootstrap'
+import "@fortawesome/fontawesome-free/js/all";
+
+//css
+import '../stylesheets/application'
