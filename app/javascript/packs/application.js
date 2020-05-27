5a7ecc541772de33e3bb2a249f2ea20ebbe23595
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-05-27 03:28:36 +0000

    Installed bootstrap and jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,14 @@ require("channels")
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Make $ available on the window object
+// for SJR views and jQuery plugins
+// that may expect `$` to be globally available.
+require('expose-loader?$!jquery')
+
+// js
+import 'bootstrap'
+import "@fortawesome/fontawesome-free/js/all";
+
+//css
+import '../stylesheets/application'
