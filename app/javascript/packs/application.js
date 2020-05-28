3650cbe635849bb2937ab2cece71e1cbbeaa3e06
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-05-28 19:20:59 +0000

    install react in rails with webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
