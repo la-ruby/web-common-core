e0c28f1d8a133e652693ffb0cfa015bbacf21d42
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-05-28 15:06:56 +0000

    Added pundit gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+  before_action :authenticate_user!
+  after_action :verify_authorized,    except: :index, except: :index, unless: :devise_controller?
+  after_action :verify_policy_scoped, only:   :index, except: :index, unless: :devise_controller?
 end
