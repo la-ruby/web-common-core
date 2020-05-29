95e64c9b48eb66e35f11016502ca74b8c3a76403
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-05-29 20:40:08 +0000

    Added gallery controller, model, views and policy

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,7 +1,10 @@
 class GalleryController < ApplicationController
+  before_action :authenticate_user!, :except => [:index]
   def index
+    @resouces = policy_scope(Gallery)
   end
 
   def page_two
+    @resouces = policy_scope(Gallery)
   end
 end
