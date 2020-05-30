544ab5c77a2291abf0fb885de2b807dceb605452
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-05-30 16:39:34 +0000

    Clean slate

--- app/jobs/example_job.rb
+++
@@ -1,9 +0,0 @@
-class ExampleJob < ApplicationJob
-  queue_as :default
-
-  # example
-  # ExampleJob.set(wait: 5.seconds).perform_later
-  def perform(*example)
-    Rails.logger.info "Performed"
-  end
-end
