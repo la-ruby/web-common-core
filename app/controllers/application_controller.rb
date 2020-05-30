52abaced971a5e5f99119977912c349b5971a898
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-05-30 14:48:00 +0000

    Clean slate

--- app/controllers/application_controller.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationController < ActionController::Base
-  include Pundit
-  before_action :authenticate_user!
-  after_action :verify_authorized, except: [:index, :page_two], unless: :devise_controller?
-  after_action :verify_policy_scoped, only: [:index, :page_two], unless: :devise_controller?
-end
