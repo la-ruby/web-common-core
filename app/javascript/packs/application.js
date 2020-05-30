fcfcd126193b5d8dd77b04d2e6eb301223551873
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-05-30 15:41:07 +0000

    Setup rendering of rails flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,7 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "controllers"
+// show flash messages
+document.addEventListener("turbolinks:load", function() {
+  $('.toast').toast('show')
+})
