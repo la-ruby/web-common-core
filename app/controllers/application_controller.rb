3eff36589c221c7bf2cd3bd34aa71a76bd7a6cf0
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-05-31 16:52:31 +0000

    Install pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+  before_action :authenticate_user!
+  after_action :verify_authorized, except: [:index], unless: :devise_controller?
+  after_action :verify_policy_scoped, only: [:index], unless: :devise_controller?
 end
