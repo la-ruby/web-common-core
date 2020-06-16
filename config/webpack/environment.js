c2a8c01b08945e9a42b957a22850c86fbd0b75d4
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-06-16 18:47:27 +0000

    Install bootstrap on rails 6 with webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,30 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+environment.loaders.append('expose', {
+    test: require.resolve('jquery'),
+    use: [
+        {
+            loader: 'expose-loader',
+            options: 'jQuery',
+        },
+        {
+            loader: 'expose-loader',
+            options: '$',
+        },
+    ],
+});
+
+        // needed?
+// https://www.vic-l.com/setup-bootstrap-in-rails-6-with-webpacker-for-development-and-production/
+//const webpack = require('webpack')
+//environment.plugins.append('Provide',
+//  new webpack.ProvidePlugin({
+//    $: 'jquery',
+//    jQuery: 'jquery',
+//    Popper: ['popper.js', 'default']
+//  })
+//)
+
+module.exports = environment
