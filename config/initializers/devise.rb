fd83bc7f5072c3f13db79594e821ff24eee31bd9
Machine User 4 <sam+machine-user-4@sam-we.com>
2020-06-28 18:22:16 +0000

    Reduce some noise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -126,7 +126,7 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
-  # config.pepper = '1b67307066e8aeaa567636e4b9514c28bcf1518d1903e048e20c63432b6c5d72ad902586fde2336e780235f7138d37bb3dbece26ac3d07ecacc1db440398166b'
+  # config.pepper= '5ba7277bf09e255a9a4cb6258a70826e9a8fd8bffc4c71984fa7686a56dd1842b4a8e6436346873e721f5b1bf63ff7b47decbec7174d72e98ff1fa5a777c86ee'
 
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
